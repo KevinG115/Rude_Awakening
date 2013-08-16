4536e9d0937c29606cff6691fe46bf3fedcf124c
kevin gleeson <kevingleeson115@gmail.com>
2013-08-16 14:23:25 +0100

    Android Alarm App

---
+++ SplashScreen/src/com/sunil/splash/Splash.java
@@ -0,0 +1,39 @@
+package com.sunil.splash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Splash extends Activity {
+	private long ms=0;
+	private long splashTime=2000;
+	private boolean splashActive = true;
+	private boolean paused=false;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		//Hides the title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		setContentView(R.layout.splash);
+
+		Thread mythread = new Thread() {
+			public void run() {
+				try {
+					while (splashActive && ms < splashTime) {
+						if(!paused)
+							ms=ms+500;
+						sleep(500);
+					}
+				} catch(Exception e) {}
+				finally {
+					Intent intent = new Intent(Splash.this, Home.class);
+					startActivity(intent);
+				}
+			}
+		};
+		mythread.start();
+	}
+}
